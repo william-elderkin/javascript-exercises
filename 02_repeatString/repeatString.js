46d4ca33a624d324511673f599a93a1db388bbc3
William Elderkin <william_elderkin@protonmail.com>
2022-05-30 19:35:13 -0400

    Work on js test answers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+/*const repeatString = function(string, num) {
+        if (num < 0) {
+            return ('ERROR');
+        }
+        return string.repeat(num); 
 };
 
 // Do not edit below this line
+module.exports = repeatString;*/
+
+
+
+const repeatString = function(string, num) {
+        if (num < 0) return 'ERROR';
+        let word = '';
+        for (let i = 0; i < num; i++) {
+            word += string;
+}
+        return word;
+}
+
 module.exports = repeatString;
